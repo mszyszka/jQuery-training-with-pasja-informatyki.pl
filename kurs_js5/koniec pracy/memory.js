3d776dcd58bab0dba85b02b933a486b4af7ac313
Michał Szyszka <mszyszka223@gmail.com>
2017-10-13 10:40:57 +0200

    I added jQuery

--- kurs_js5/koniec pracy/memory.js
+++ kurs_js5/koniec pracy/memory.js
@@ -37,88 +37,14 @@ c11.addEventListener("click", function() { revealCard(11); });
 
 var oneVisible = false;
 var turnCounter = 0;
-var visible_nr;
-var lock = false;
-var pairsLeft = 6;
+
 
 function revealCard(nr)
 {
-	var opacityValue = $('#c'+nr).css('opacity');
-	
-	//alert('Opacity: '+opacityValue);
-	
-	if (opacityValue != 0 && lock == false)
-	{
-		lock = true;
-		
-		//alert(nr);
-	
 		var obraz = "url(img/" + cards[nr] + ")";
-		
 		$('#c'+nr).css('background-image', obraz);
-		$('#c'+nr).addClass('cardA');
-		$('#c'+nr).removeClass('card');
 		
-		if(oneVisible == false)
-		{
-			//first card
-			
-			oneVisible = true;
-			visible_nr = nr;
-			lock = false;
-		}
-		else
-		{
-			//second card
-			
-			if(cards[visible_nr] == cards[nr])
-			{
-				//alert("para");
-				
-				setTimeout(function() { hide2Cards(nr, visible_nr) }, 750);
-				
-			}
-			else
-			{
-				//alert("pudło");
-				
-				setTimeout(function() { restore2Cards(nr, visible_nr) }, 1000);
-			}
-			
-			turnCounter++;
-			$('.score').html('Turn counter: '+turnCounter);
-			oneVisible = false;
-		}
-		
-	}
-	
-}
 
-function hide2Cards(nr1, nr2)
-{
-	$('#c'+nr1).css('opacity', '0');
-	$('#c'+nr2).css('opacity', '0');
 	
-	pairsLeft--;
-	
-	if(pairsLeft == 0)
-	{
-		$('.board').html('<h1>You win!<br>Done in '+turnCounter+' turns</h1>');
-	}
-	
-	lock = false;
-}
-
-function restore2Cards(nr1, nr2)
-{
-	$('#c'+nr1).css('background-image', 'url(img/karta.png)');
-	$('#c'+nr1).addClass('card');
-	$('#c'+nr1).removeClass('cardA');	
-
-	$('#c'+nr2).css('background-image', 'url(img/karta.png)');
-	$('#c'+nr2).addClass('card');
-	$('#c'+nr2).removeClass('cardA');
-	
-	lock = false;
 }
 
